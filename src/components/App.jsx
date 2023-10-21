36843262a94cc7e6826dbc08940f1343c812dd39
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-10-21 09:12:08 +0300

    add component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import CircleCountDown from './CircleCountDown/CircleCountDown';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+        <CircleCountDown
+          timeInSecond={65}
+          size={200}
+          strokeBgColor="black"
+          strokeColor="lightcoral"
+          strokeWidth={4}
+          tipWidth={20}
+        >
+          {elem => {
+            console.log(elem);
+            return <div>{-elem.angleRotation}</div>;
+          }}
+        </CircleCountDown>
+      </div>
+    </>
   );
 };
